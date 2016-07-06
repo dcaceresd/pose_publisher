25a83a4dd87031c2c185324be8d9323beefe4602
dcaceresd <100291267@alumnos.uc3m.es>
2016-07-06 13:38:16 +0200

    Update pose_transform_node.py
    Indentation fixes

--- src/pose_transform_node.py
+++ src/pose_transform_node.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python 
 
-"""Module to publish the necesary user data
-"""
+"""Module to publish the necesary user data"""
 
 import roslib
 roslib.load_manifest('pose_publisher')
@@ -18,24 +17,24 @@ if __name__ == '__main__':
 	"""Creates an array to store the last posture of each user"""
 	lastPosture = []
 	for i in range(users):
-   		lastPosture.append([])
-    		for j in range(15):
-       			lastPosture[i].append(None)
-
-	rate = rospy.Rate(1)    
-
+		lastPosture.append([])
+		for j in range(15):
+			lastPosture[i].append(None)
+			
+	rate = rospy.Rate(1)
+	
 	while not rospy.is_shutdown():
 		try:
 			for i in range(users):
 				pos.setUser(str(i+1))
-
+				
 				if pos.userExistance():
 					if not (lastPosture[i] == pos.getPosture()):
 						rospy.loginfo('User: ' + str(i+1) + ' moving!')	
-
+						
 					lastPosture[i] = pos.getPosture()
 					
 			rate.sleep()
-
+			
 		except:
 			rospy.loginfo('Unexpected error')
