fe5aa171e2f40af6c7ab609ebe3ba47a4489701f
dcaceresd <100291267@alumnos.uc3m.es>
2016-08-05 17:15:04 +0200

    Update pose_transform_node.py

--- src/pose_transform_node.py
+++ src/pose_transform_node.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 
 """Module to publish the necesary user data"""
 
@@ -7,34 +7,44 @@ roslib.load_manifest('pose_publisher')
 import rospy
 from kinect_posture import User
 
+
 if __name__ == '__main__':
 
 	pos = User()
 	rospy.loginfo('Initializing node...')
 
 	users = 10
+
+	v = Vector3()
+	v.x = 0
+	v.y = 0
+	v.z = 0
+
+	m = [v, v]
+	
 	
 	"""Creates an array to store the last posture of each user"""
 	lastPosture = []
 	for i in range(users):
-		lastPosture.append([])
-		for j in range(15):
-			lastPosture[i].append(None)
-			
-	rate = rospy.Rate(1)
-	
+   		lastPosture.append([])
+    		for j in range(15):
+       			lastPosture[i].append(None)
+
+	"""Array to store the past velocities"""
+	velocities = []
+ 
+	rate = rospy.Rate(2)    
+
 	while not rospy.is_shutdown():
-		try:
-			for i in range(users):
-				pos.setUser(str(i+1))
-				
-				if pos.userExistance():
-					if not (lastPosture[i] == pos.getPosture()):
-						rospy.loginfo('User: ' + str(i+1) + ' moving!')	
-						
-					lastPosture[i] = pos.getPosture()
-					
-			rate.sleep()
-			
-		except:
-			rospy.loginfo('Unexpected error')
+		for i in range(users):
+			pos.setUser(str(i+1))
+
+			if pos.userExistance():
+				if not (lastPosture[i] == pos.getPosture()):
+					rospy.loginfo('User: ' + str(i+1) + ' moving!')
+					velocities.append(pos.getVelocities())
+					print pos.getVelocities()
+				else:
+					velocities.append(m)
+				lastPosture[i] = pos.getPosture()
+		rate.sleep()
